e08909f2b7f1f13f95900ebadc7e7a46c0612105
Văn Duy <vanduy@MacBook-Pro-cua-Van.local>
2024-11-18 20:52:16 +0700

    (feature): create register page

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -1,12 +1,15 @@
 const authenticationService = require('../services/authentication.service');
 
+async function getRegisterPage(req, res, next) {
+  return await authenticationService.getRegisterPage(req, res);
+}
+
+async function register(req, res, next) {
+  return await authenticationService.register(req, res)
+}
+
 async function getLoginPage(req, res, next) {
-  try {
-    return await authenticationService.getLoginPage(req, res)
-  } catch (err) {
-    console.error('Error while get login page: ', err.message)
-    next(err)
-  }
+  return await authenticationService.getLoginPage(req, res)
 }
 
 async function login(req, res, next) {
@@ -30,5 +33,7 @@ async function logout(req, res, next) {
 module.exports = {
   getLoginPage, 
   login,
-  logout
+  logout,
+  getRegisterPage,
+  register
 }
--- routes/authentication.router.js
+++ routes/authentication.router.js
@@ -4,6 +4,8 @@ const router = Router();
 const authenticationController = require('../controllers/authentication.controller')
 
 //authentications
+router.get('/register', authenticationController.getRegisterPage);
+router.post('/register', authenticationController.register);
 router.get('/login', authenticationController.getLoginPage);
 router.post('/login', authenticationController.login);
 router.get('/logout', authenticationController.logout);
